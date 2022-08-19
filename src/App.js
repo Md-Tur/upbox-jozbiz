52d9c166b95090e55fd23f7c5ded2b19390b8918
Md-Tur <turibnmostafiz@gmail.com>
2022-08-19 23:51:15 +0600

    social section added

--- src/App.js
+++ src/App.js
@@ -70,7 +70,128 @@ function App() {
         </div>
       </div>
 
+      {/* Plan card section */}
+      <div className='container'>
+        <div className='d-flex mx-auto mb-5'>
+          <div className='mx-auto mt-5'>
+            <h1>Pick Your Plan</h1>
+            <img src={underline} alt="" />
+          </div>
+        </div>
 
+        <div className='row'>
+          <div className='col-lg-6 col-md-6 col-sm-12'>
+            <div className="card" style={{ width: "18rem" }}>
+              <div className="card-body text-center">
+                <h5 className="card-title">Monthly</h5>
+                <p className="card-text">$18/mo</p>
+                <div className='d-flex align-items-center justify-content-center'>
+                  <a style={{ backgroundColor: "#EF8094" }} href="#" className="btn btn-primary rounded-pill px-3 border-0">FIND YOUR BOX</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='col-lg-6 col-md-6 col-sm-12'>
+            <div className="card" style={{ width: "18rem" }}>
+              <div className="card-body text-center">
+                <h5 className="card-title">Yearly</h5>
+                <p className="card-text">$15/mo</p>
+                <div className='d-flex align-items-center justify-content-center'>
+                  <a style={{ backgroundColor: "#EF8094" }} href="#" className="btn btn-primary rounded-pill px-3 border-0">FIND YOUR BOX</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <p className='text-center mt-4 mb-5'>Your plan auto-renews at the end of 30 days from the 1st day you signed-up.</p>
+      </div>
+
+      {/* works section */}
+      <div className='container'>
+        <div className='d-flex mx-auto'>
+          <div className='mx-auto mt-3 mb-3'>
+            <h1>How it works?</h1>
+            <img src={underline1} alt="" />
+          </div>
+        </div>
+
+        <div className='d-flex mb-3'>
+          <img src={girl1} alt="" />
+          <div className='d-flex align-items-center ms-4'>
+            <div>
+              <h2>Setup your profile &
+                preferences</h2>
+              <p>Once you create an account, you can start to tell us your likes and dislikes so we can tailor the experience just for you.</p>
+            </div>
+          </div>
+        </div>
+        <div className='mb-5'>
+          <img className='img-fluid' src={box1} alt="" />
+          <div>
+            <h2>Setup your profile &
+              preferences</h2>
+            <p>Once you create an account, you can start to tell us your likes and dislikes so we can tailor the experience just for you.</p>
+          </div>
+        </div>
+        <div className='d-flex'>
+          <img src={girl2} alt="" />
+          <div className='d-flex align-items-center ms-4'>
+            <div>
+              <h2>Setup your profile &
+                preferences</h2>
+              <p>Once you create an account, you can start to tell us your likes and dislikes so we can tailor the experience just for you.</p>
+            </div>
+          </div>
+        </div>
+        <div className='d-flex align-items-center justify-content-center mt-5 mb-5'>
+          <a style={{ backgroundColor: "#FFFFFF", borderColor: "#EF8094", color: "black" }} href="#" className="btn btn-primary rounded-pill px-3">TRY IT FOR YOURSELF<i className="fa-solid fa-angle-right ms-2"></i></a>
+        </div>
+      </div>
+
+      {/* Social posts section */}
+      <div style={{ backgroundColor: "#F5F8FF" }} className="mt-5 mb-3">
+        <h1 className='text-center pt-5 pb-3'>Social Posts</h1>
+        <div className='row mx-3'>
+          <div className='col-lg-3 col-md-6 col-sm-12'>
+            <div className="card" style={{ width: "18rem" }}>
+              <img src={img1} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <p className="card-text">How to rock the lip look that turns heads</p>
+              </div>
+            </div>
+          </div>
+          <div className='col-lg-3 col-md-6 col-sm-12'>
+            <div className="card" style={{ width: "18rem" }}>
+              <img src={img2} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <p className="card-text">Find the perfect shade for the season</p>
+              </div>
+            </div>
+          </div>
+
+          <div className='col-lg-3 col-md-6 col-sm-12'>
+            <div className="card" style={{ width: "18rem" }}>
+              <img src={img3} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <p className="card-text">The 5 eye shadow secrets you never knew</p>
+              </div>
+            </div>
+          </div>
+
+          <div className='col-lg-3 col-md-6 col-sm-12'>
+            <div className="card" style={{ width: "18rem" }}>
+              <img src={img4} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <p className="card-text">The pro-tips for at home hair dying</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div className='d-flex align-items-center justify-content-center'>
+          <a style={{ backgroundColor: "#EF8094" }} href="#" className="btn btn-primary rounded-pill px-3 border-0 mt-5 mb-5">FOLLOW US ON INSTAGRAM</a>
+        </div>
+      </div>
 
     </div>
   );
